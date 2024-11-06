87ced8ad3ca9a2823a492a60410f9465912864ca
FacuMartine <facu.martinezduarte@correo.ucu.edu.uy>
2024-11-06 11:50:51 -0300

    Items todo terminao

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba496afb0521fbc41fccdcba41148892fb2b26f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee614ab96da952355b8f4586509597f7e39a6558")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
