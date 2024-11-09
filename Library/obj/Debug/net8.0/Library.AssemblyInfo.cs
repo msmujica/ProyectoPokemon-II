ef2e0138000dd92d443426c47db589fcb7b17538
Martin <mujica0106@gmail.com>
2024-11-08 22:51:59 -0300

    Se agrega DOCS

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6b831d30a3151cc5df8c98986e21df7c36c005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debc784659809e6d1d8baa7713fa864ea2fbaa0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
