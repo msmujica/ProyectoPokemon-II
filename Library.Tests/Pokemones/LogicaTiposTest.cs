88b5eda9e7bf63e86f60f427c0dee968a8417137
Matias Hernandez <matiashernandezdonbosco@gmail.com>
2024-11-11 11:57:53 -0300

    Comentarios de LogicaTiposTest en proceso

--- Library.Tests/Pokemones/LogicaTiposTest.cs
+++ Library.Tests/Pokemones/LogicaTiposTest.cs
@@ -5,11 +5,14 @@ namespace Library.Tests
 {
     [TestClass]
     public class LogicaTiposTests
-    {
+    {   
+        // Se prueban debilidades, en este caso se prueba que el tipo Fuego es efectivo contra el tipo Planta
         [TestMethod]
         public void Debilidades()
-        {
+        {      
+            //Act: Se calcula el multiplicador para un ataque tipo fuego contra un Pokemón tipo planta
             double multiplicador = LogicaTipos.CalcularMultiplicador("Fuego", "Planta");
+            //Assert: El multilplicador debe ser 2, indicando que el ataque es superefectivo
             Assert.AreEqual(2, multiplicador, "El ataque de tipo Fuego debería ser super efectivo contra tipo Planta.");
         }
 
@@ -19,8 +22,9 @@ namespace Library.Tests
             double multiplicador = LogicaTipos.CalcularMultiplicador("Fuego", "Agua");
             Assert.AreEqual(0.5, multiplicador, "El ataque de tipo Fuego debería ser poco efectivo contra tipo Agua.");
         }
-
+        
         [TestMethod]
+        //En este caso se verifica
         public void Inmunidades()
         {
             double multiplicador = LogicaTipos.CalcularMultiplicador("Eléctrico", "Tierra");
